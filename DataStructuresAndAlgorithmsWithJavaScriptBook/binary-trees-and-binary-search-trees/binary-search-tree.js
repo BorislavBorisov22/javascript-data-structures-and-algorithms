4b1f0aa9b6e2f14cb6df8a6dde62f50976d3f6fd
BorislavBorisov22 <borislavborisov22@abv.bg>
2017-12-31 17:53:28 +0200

    Added remove method to the binary search tree

--- DataStructuresAndAlgorithmsWithJavaScriptBook/binary-trees-and-binary-search-trees/binary-search-tree.js
+++ DataStructuresAndAlgorithmsWithJavaScriptBook/binary-trees-and-binary-search-trees/binary-search-tree.js
@@ -192,7 +192,7 @@ class BinarySearchTree {
     find(data) {
         let current = this.root;
 
-        while (current.data !== data) {
+        while (!!current && current.data !== data) {
             if (this.compareFunc(data, current.data) < 0) {
                 current = current.left;
             } else {
@@ -206,20 +206,97 @@ class BinarySearchTree {
 
         return current;
     }
+
+    /**
+     * Returns the smallest node of a subtree with a root - the passed
+     * node parameter
+     * 
+     * @param {Node} node the node to start searching from
+     * @returns {Node} Returns the smallest node from a subtree
+     *
+     * 
+     * @memberOf BinarySearchTree
+     */
+    getSmallest(node) {
+        if (!node) {
+            throw new Error('Passed node cannot be undefined!');
+        }
+
+        let smallestData = Number.MAX_VALUE;
+        let smallestNode = null;
+        let current = node;
+
+        while (current !== null) {
+            if (this.compareFunc(current.data, smallestData) < 0) {
+                smallestData = current.data;
+                smallestNode = current;
+            }
+
+            current = current.left;
+        }
+
+        return smallestNode;
+    }
+
+    /**
+     * Removes the first occurence of a node with the data specified
+     * parameter
+     * 
+     * @param {any} data 
+     * 
+     * @memberOf BinarySearchTree
+     */
+    remove(data) {
+        this.root = this.removeNode(this.root, data);
+    }
+
+    /**
+     * Removes the first found occurence of a node with the specified
+     * data parameter
+     * 
+     * @param {Node} node Current node
+     * @param {any} data Target data to be removed
+     * @returns {Node}
+     * 
+     * @memberOf BinarySearchTree
+     */
+    removeNode(node, data) {
+        if (node === null) {
+            return null;
+        }
+
+        const compareResult = this.compareFunc(data, node.data);
+        if (compareResult === 0) {
+            if (node.left === null && node.right === null) {
+                return null;
+            }
+
+            if (node.left === null) {
+                return node.right;
+            }
+
+            if (node.right === null) {
+                return node.left;
+            }
+
+            const tempNode = this.getSmallest(node.right);
+            node.data = tempNode.data;
+
+            node.right = this.removeNode(node.right, tempNode.data);
+            return node;
+        } else if (compareResult < 0) {
+            node.left = this.removeNode(node.left, data);
+            return node;
+        } else {
+            node.right = this.removeNode(node.right, data);
+            return node;
+        }
+    }
 }
 
 const tree = new BinarySearchTree((a, b) => a - b);
-tree.insert(22);
-tree.insert(44);
-tree.insert(20000);
-tree.insert(99);
-tree.insert(30);
-tree.insert(28);
-tree.insert(30)
-
-const node = tree.find(30);
-
-console.log(node);
-console.log(`Max: ${tree.getMax()}`, `Min: ${tree.getMin()}`);
+tree.insertMany(7, 6, 19, 4, 6, 6.5, 6.5, 10, 9, 25, 17, 13, 12, 15);
+tree.remove(10);
+console.log(tree.find(17));
 
 module.exports = BinarySearchTree;
