0bbafc767b6f0b8c184f0658806f60b78b58aab2
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-01-16 23:58:12 +0200

    Added performance test case for already sorted data

--- DataStructuresAndAlgorithmsWithJavaScriptBook/sorting/sorting-performance.js
+++ DataStructuresAndAlgorithmsWithJavaScriptBook/sorting/sorting-performance.js
@@ -24,20 +24,35 @@ const sortingMethodsNames = ['bubbleSort', 'selectionSort', 'insertionSort', 'sh
 /*1. Run the three algorithms discussed in this chapter with string data rather than
 numeric data and compare the running times for the different algorithms. Are the
 results consistent with the results of using numeric data? */
-testSortingPerformance(1000, (helper) => {
-    let strings = ['randomString', 'someOtherRandomString', 'moreRandomTextHere', 'andSomeMore'];
-    for (let i = 0; i < 11; ++i) {
-        strings = [...strings, ...strings];
-    }
+const sortWithStrings = () => {
+    testSortingPerformance(1000, (helper) => {
+        let strings = ['randomString', 'someOtherRandomString', 'moreRandomTextHere', 'andSomeMore'];
+        for (let i = 0; i < 11; ++i) {
+            strings = [...strings, ...strings];
+        }
 
-    helper.dataStore = strings;
+        helper.dataStore = strings;
 
-}, sortingMethodsNames, 'Sorting String values', (a, b) => {
-    if (a < b) {
-        return -1;
-    } else if (a > b) {
-        return 1;
-    }
+    }, sortingMethodsNames, 'Sorting String values', (a, b) => {
+        if (a < b) {
+            return -1;
+        } else if (a > b) {
+            return 1;
+        }
 
-    return 0;
-});
+        return 0;
+    });
+};
+
+/* 2. Create an array of 1,000 integers already sorted into numeric order. Write a program
+that runs each sorting algorithm with this array, timing each algorithm and com‐
+paring the times. How do these times compare to the times for sorting an array in
+random order? */
+const sortAlreadySortedElements = () => {
+    testSortingPerformance(1000, (helper) => {
+        helper.setData();
+        helper.mergeSort();
+    }, sortingMethodsNames, 'Sorting already sorted data');
+};
+
+// sortAlreadySortedElements();
