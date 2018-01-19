68abef433e76a0fc453e3830fedddc696e3574e5
Borislav Borisov <borislavborisov22@abv.bg>
2018-01-19 18:51:43 +0200

    Added coin change problem greedy solution

---
+++ DataStructuresAndAlgorithmsWithJavaScriptBook/advanced-algorithms/greedy-algorithms/coin-change.js
@@ -0,0 +1,28 @@
+const makeChange = (originalAmount) => {
+    // saving each of the available coins count needed to make the change
+    // indeces => 0 -> 0.1; 1 -> 0.05; 2 -> 0.1; 3 -> 0.25;
+    const coins = Array.from({ length: 4 });
+
+    let remainingAmount = 0;
+    if (originalAmount % 0.25 < originalAmount) {
+        coins[3] = parseInt(originalAmount / 0.25);
+        remainingAmount = originalAmount % 0.25;
+        originalAmount = remainingAmount;
+    }
+
+    if (originalAmount % 0.1 < originalAmount) {
+        coins[2] = parseInt(originalAmount / 0.1);
+        remainingAmount = originalAmount % 0.1;
+        originalAmount = remainingAmount;
+    }
+
+    if (originalAmount % 0.05 < originalAmount) {
+        coins[1] = parseInt(originalAmount / 0.05);
+        remainingAmount = originalAmount % 0.05;
+        originalAmount = remainingAmount;
+    }
+
+    coins[0] = parseInt(originalAmount / 0.01);
+
+    return coins;
+}
