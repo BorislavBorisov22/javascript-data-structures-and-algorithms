f85cba2f985554c8c980b5570fe991aea4bc3f9c
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-01-21 13:55:01 +0200

    Added docs for the knapsackFraction problem

--- DataStructuresAndAlgorithmsWithJavaScriptBook/advanced-algorithms/greedy-algorithms/knapsack-fractional.js
+++ DataStructuresAndAlgorithmsWithJavaScriptBook/advanced-algorithms/greedy-algorithms/knapsack-fractional.js
@@ -1,3 +1,20 @@
+const max = (a, b) => a > b ? a : b;
+
+/**
+ * Solves science Knapsack fractional problem which defers from the
+ * classical knapsack problem by allowing us to select only a portion
+ * of an item if needed.
+ * 
+ * 
+ * @param {Number} capacity the maximum capacity that can be taken
+ * @param {Array} weights the weights of the items to select from
+ * @param {Array} values the values of the items to select from
+ * @param {Number} n the number of all items to select from
+ * @returns {Number} The maximum value that can be achieved from
+ * selecting the provided items without taking elements with summed
+ * weight more than the provided capacity. With the fractional problem
+ * we can take only parts of some items when we cannot take a whole item
+ */
 const knapsackFraction = (capacity, weights, values, n) => {
     let load = 0;
     let index = 0;
