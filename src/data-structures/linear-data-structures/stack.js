74729752661803a8ce3a1fbb029cabd30d7c74cf
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-01-24 00:03:46 +0200

    Added docs for the stack implementation

--- src/data-structures/linear-data-structures/stack.js
+++ src/data-structures/linear-data-structures/stack.js
@@ -1,10 +1,39 @@
-((exports) => {
+/**
+ * Stack data structure
+ *
+ *
+ * @example
+ * var stackModule = require('path-to-algorithms/src/data-structures'+
+ * 'linear-data-structures/stack');
+ * var stack = new stackModule.Stack();
+ *
+ * stack.push(1996);
+ * stack.peek();
+ * stack.pop();
+ *
+ * @module data-structures/linear-data-structures-avl-tree
+ */
 
+(function(exports) {
+
+    /**
+     * Stack.
+     *
+     * @public
+     * @constructor
+     */
     exports.Stack = function() {
         this._data = [];
         this.size = 0;
     };
 
+    /**
+     * Adds element on the top of the stack.
+     *
+     * @public
+     * @method
+     * @param {Any} value value to add to the stack.
+     */
     exports.Stack.prototype.push = function(value) {
         if (typeof value === 'undefined') {
             throw new Error('Cannot add undefined value!');
@@ -13,25 +42,49 @@
         this._data[this.size++] = value;
     };
 
+    /**
+     * Removes the elements from the top of the stack
+     * and returns the removed element.
+     *
+     * @public
+     * @method
+     * @returns {Any} the value popped from the top of the stack
+     */
     exports.Stack.prototype.pop = function() {
         if (!this.size === 0) {
             return null;
         }
 
-        const valueToReturn = this.peek();
+        var valueToReturn = this.peek();
         this._data.splice(this.size--, 1);
 
         return valueToReturn;
-    }
+    };
 
+    /**
+     * Returns the value on top of the stack
+     * without removing it.
+     *
+     * @public
+     * @method
+     * @returns {Any} the value from the top
+     * of the stack or null if stack is empty
+     */
     exports.Stack.prototype.peek = function() {
         return this._data[this.size - 1];
-    }
+    };
 
+    /**
+     * Returns wheter the stack is empty or not.
+     *
+     * @public
+     * @property
+     * @returns {Any} the value from the top
+     * of the stack or null if stack is empty
+     */
     Object.defineProperty(exports.Stack.prototype, 'isEmpty', {
         get: function() {
             return this.size === 0;
         },
     });
-
 })(typeof window === 'undefined' ? module.exports : window);
