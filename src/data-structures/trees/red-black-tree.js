0e674c7dc3fbecd60ec79ae83b67ad2846c62909
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-02-11 00:20:53 +0200

    Added red-black tree insertion and balancing

--- src/data-structures/trees/red-black-tree.js
+++ src/data-structures/trees/red-black-tree.js
@@ -5,38 +5,48 @@
         RED: true
     };
 
-    const RedBlackNode = function(value) {
+    const Node = function(value) {
         this.value = value;
-        this.color = color.RED;
         this.left = null;
         this.right = null;
+        this.count = 1;
+        this.color = color.RED;
     };
 
-    RedBlackNode.rotateRight = function(node) {
-        const newNode = node.left;
-        node.left = newNode.right;
-        newNode.right = node;
-
-        return node;
+    Node.count = function(node) {
+        return node === null ? 0 : node.count;
     };
 
-    RedBlackNode.rotateLeft = function(node) {
+    Node.isRed = function(node) {
+        return node === null ? false : node.color === color.RED;
+    };
+
+    Node.rotateLeft = function(node) {
         const newNode = node.right;
         node.right = newNode.left;
         newNode.left = node;
 
+        newNode.color = node.color;
+        node.color = color.RED;
+
         return newNode;
     };
 
-    RedBlackNode.flipColors = function(node) {
-        node.color = color.RED;
-        if (node.left) {
-            node.left.color = color.BLACK;
-        }
+    Node.rotateRight = function(node) {
+        const newNode = node.left;
+        node.left = newNode.right;
+        newNode.right = node;
 
-        if (node.right) {
-            node.right.color = color.BLACK;
-        }
+        newNode.color = node.color;
+        node.color = color.RED;
+
+        return newNode;
+    };
+
+    Node.flipColors = function(node) {
+        node.color = color.RED;
+        node.left.color = color.BLACK;
+        node.right.color = color.BLACK;
 
         return node;
     };
@@ -46,10 +56,62 @@
         this.root = null;
     };
 
-    RedBlackTree.prototype._isRed = function(node) {
-        return node.color === color.RED;
+    RedBlackTree.prototype.insert = function(value) {
+        this.root = this._insert(this.root, value);
+        this.root.color = color.BLACK;
+    };
+
+    RedBlackTree.prototype._insert = function(node, value) {
+        if (node === null) {
+            const newNode = new Node(value);
+            return newNode;
+        }
+
+        const compareResult = this.cmp(value, node.value);
+        if (compareResult < 0) {
+            node.left = this._insert(node.left, value);
+        } else if (compareResult > 0) {
+            node.right = this._insert(node.right, value);
+        } else {
+            // just skip if value is already present;
+            return node;
+        }
+
+        if (Node.isRed(node.right) && !Node.isRed(node.left)) {
+            node = Node.rotateLeft(node);
+        }
+
+        if (Node.isRed(node.left) && Node.isRed(node.left.left)) {
+            node = Node.rotateRight(node);
+        }
+
+        if (Node.isRed(node.left) && Node.isRed(node.right)) {
+            node = Node.flipColors(node);
+        }
+
+        return node;
+    };
+
+    RedBlackTree.prototype.inOrder = function(callback) {
+        this._inOrder(this.root, callback);
+    };
+
+    RedBlackTree.prototype._inOrder = function(node, callback) {
+        if (node === null) {
+            return;
+        }
+
+        this._inOrder(node.left);
+        callback(node);
+        this._inOrder(node.right);
     };
 
     exports.RedBlackTree = RedBlackTree;
 
 })(typeof window === 'undefined' ? module.exports : window);
+
+const tree = new module.exports.RedBlackTree((a, b) => a - b);
+const values = [30, 20, 40, 35, 37, 45, 44, 50, 47];
+values.forEach(value => tree.insert(value));
+
+console.log(tree);
