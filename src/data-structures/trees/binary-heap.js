acf4f1e1b13f16ebcef2649219b54782b828796e
BorislavBorisov22 <borislavborisov22@abv.bg>
2018-04-30 18:23:44 +0300

    added new binary heap implementation with es6 classes

--- src/data-structures/trees/binary-heap.js
+++ src/data-structures/trees/binary-heap.js
@@ -1,89 +1,72 @@
-(function(exports) {
-    const BinaryHeap = function(cmp) {
-        if (typeof cmp !== 'function' || cmp.length < 2) {
-            throw new Error('Passed cmp method must be of type function accepting two parameters!');
-        }
 
-        this.cmp = cmp;
-        this._data = [null];
+
+const defaultComparator = (a, b) => a - b;
+
+class BinaryHeap {
+    constructor(cmp) {
+        this.cmp = cmp || defaultComparator;
         this.size = 0;
-    };
+        this.data = [null];
+    }
 
-    BinaryHeap.prototype.enqueue = function(value) {
+    insert(value) {
         if (typeof value === 'undefined') {
-            throw new Error('cannot enqueue an undefined value!');
+            throw new TypeError('Passed value must be defined');
         }
 
-        var index = this._data.length;
-        while (index > 1 && this.cmp(value, this._data[Math.floor(index / 2)]) < 0) {
-            this._data[index] = this._data[Math.floor(index / 2)];
-            index = Math.floor(index / 2);
+        let index = this.data.length / 2;
+
+        while (parseInt(index / 2 > 1) && this.cmp(value, this.data[parseInt(index / 2)]) < 0) {
+            this.data[index] = this.data[parseInt(index / 2)];
+            index = parseInt(index / 2);
         }
 
-        ++this.size;
-        this._data[index] = value;
-    };
+        this.data[index] = value;
+        this.size++;
+    }
 
-    BinaryHeap.prototype.enqueueMany = function(...values) {
-        values.forEach(v => this.enqueue(v));
-    };
-
-    BinaryHeap.prototype.dequeue = function() {
-        if (this.isEmpty) {
-            return null;
+    removeTop() {
+        if (this.isEmpty()) {
+            throw new Error('No elements to remove from heap!');
         }
 
-        var valueToReturn = this._data[1];
+        const returnValue = this.data[1];
+        const value = this.data[this.size - 1];
+        this.data.splice(this.size, 1);
+        this.size--;
 
-        var value = this._data[this._data.length - 1];
-        this._data.splice(this._data.length - 1, 1);
-        --this.size;
+        let index = 1;
+        while (index * 2 + 1 < this.data.length) {
+            let smallerKidIndex = this.cmp(this.data[index * 2], this.data[index * 2 + 1]) < 0 ?
+                index * 2 : index * 2 + 1;
 
-        if (!this.isEmpty) {
-            var index = 1;
-            this._heapifyDown(index, value);
-        }
-
-        return valueToReturn;
-    };
-
-    BinaryHeap.prototype._heapifyDown = function(index, value) {
-        if (Number.isNaN(index) || index < 0 || index > this.size) {
-            throw new Error('Passed index must be a number in the range of the heap size!');
-        }
-
-        if (typeof value === 'undefined') {
-            throw new Error('Passed value parameter cannot be undefined!');
-        }
-
-        while (index * 2 + 1 < this._data.length) {
-            var smallerKidIndex =
-                this.cmp(this._data[index * 2], this._data[index * 2 + 1]) < 0 ? index * 2 : index * 2 + 1;
-
-            if (this.cmp(this._data[smallerKidIndex], value) < 0) {
-                this._data[index] = this._data[smallerKidIndex];
+            const compareResult = this.cmp(value, this.data[smallerKidIndex]);
+            if (compareResult > 0) {
+                this.data[index] = this.data[smallerKidIndex];
                 index = smallerKidIndex;
             } else {
                 break;
             }
         }
 
-        if (index * 2 < this._data.length) {
-            if (this.cmp(this._data[index * 2], value) < 0) {
-                this._data[index] = this._data[index * 2];
-                index *= 2;
+        if (index * 2 < this.data.length) {
+            let smallerKidIndex = index * 2;
+            const compareResult = this.cmp(value, this.data[smallerKidIndex]);
+
+            if (compareResult > 0) {
+                this.data[index] = this.data[smallerKidIndex];
+                index = smallerKidIndex;
             }
         }
 
-        this._data[index] = value;
-    };
+        this.data[index] = value;
 
-    Object.defineProperty(BinaryHeap.prototype, 'isEmpty', {
-        get: function() {
-            return this.size === 0;
-        }
-    });
+        return returnValue;
+    }
 
-    exports.BinaryHeap = BinaryHeap;
+    isEmpty() {
+        return this.size === 1;
+    }
+}
 
-})(typeof window === 'undefined' ? module.exports : window);
+module.exports = BinaryHeap;
